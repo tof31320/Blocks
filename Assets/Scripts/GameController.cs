e95324698e3b276d6467e54a91b7885a2da88ed0
chris <christophe.leblond@inp-toulouse.fr>
2016-03-04 17:12:07 +0100

    WIP

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+    private static GameController _instance = null;
+    public static GameController instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
+            }
+            return _instance;
+        }
+    }
+
+    public Grille grille;
+
+    public void Start()
+    {
+        ResetGrille();
+    }
+
+    private void ResetGrille()
+    {
+        grille.Build();
+    }
+}
---
+++ Assets/Scripts/Grille.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Grille : MonoBehaviour {
+
+    private static Grille _instance = null;
+    public static Grille instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = GameObject.FindGameObjectWithTag("Grille").GetComponent<Grille>();
+            }
+            return _instance;
+        }
+    }
+
+    public Vector2 dimensions = Vector2.zero;
+
+    public GameObject cellPrefab;
+
+    public float CELL_SIZE = 0.5f;
+
+    public List<Cell> cells = new List<Cell>();
+
+    public void Build()
+    {
+        cells = new List<Cell>();
+
+        for (int i = 0; i < dimensions.x; i++)
+        {
+            for(int j = 0; j < dimensions.y; j++)
+            {
+                Cell cell = CreateAndCell(i, j);
+                cells.Add(cell);
+            }
+        }
+    }    
+
+    public Cell CreateAndCell(int i, int j)
+    {
+        Vector3 cellPosition = new Vector3(transform.position.x + i * CELL_SIZE,
+                                           transform.position.y,
+                                           transform.position.z + j * CELL_SIZE);
+
+        GameObject g = Instantiate(cellPrefab, cellPosition, Quaternion.identity) as GameObject;
+        g.transform.SetParent(transform);
+        Cell cell = g.GetComponent<Cell>();
+
+        return cell;
+    }
+}
